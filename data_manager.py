9f357e9c5ec78d4c58d4ab8e909e3774e31e3297
Claudiu <cduneata@gmail.com>
2019-12-27 16:43:03 +0200

    Add logic to show all mentors

--- data_manager.py
+++ data_manager.py
@@ -10,3 +10,15 @@ def get_mentor_names_by_first_name(cursor, first_name):
                    {'first_name': first_name})
     names = cursor.fetchall()
     return names
+
+@database_common.connection_handler
+def get_mentors_names(cursor):
+    cursor.execute(
+                    """
+                    SELECT first_name, last_name FROM mentors;
+                    """
+                  )
+
+    first_and_last_name = cursor.fetchall()
+    return first_and_last_name
+
