32bc21298cc2061f838f2fcf7abaae8ef322637c
Claudiu <cduneata@gmail.com>
2019-12-27 16:51:54 +0200

    Add nick_name query

--- data_manager.py
+++ data_manager.py
@@ -11,14 +11,23 @@ def get_mentor_names_by_first_name(cursor, first_name):
     names = cursor.fetchall()
     return names
 
+
 @database_common.connection_handler
 def get_mentors_names(cursor):
     cursor.execute(
-                    """
+        """
                     SELECT first_name, last_name FROM mentors;
                     """
-                  )
+    )
 
     first_and_last_name = cursor.fetchall()
     return first_and_last_name
+
+@database_common.connection_handler
+def get_nickname_miskolc(cursor):
+    cursor.execute("""SELECT nick_name FROM mentors
+                      WHERE city = 'Miskolc';""")
+
+    mentors_nickname = cursor.fetchall()
+    return mentors_nickname
 
