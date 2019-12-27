050c21ec3a9080dd76d5f2bd1e42cead0394c9e4
Claudiu <cduneata@gmail.com>
2019-12-27 16:43:14 +0200

    Add route to all mentor names

--- main.py
+++ main.py
@@ -13,8 +13,13 @@ def index():
 def mentor_names():
     # We get back dictionaries here (for details check 'database_common.py')
     mentor_names = data_manager.get_mentor_names_by_first_name('László')
-
     return render_template('mentor_names.html', mentor_names=mentor_names)
+
+
+@app.route('/all-mentors')
+def all_names():
+    first_and_last_name = data_manager.get_mentors_names()
+    return render_template('first_second_name.html', first_and_last_name=first_and_last_name)
 
 
 if __name__ == '__main__':
